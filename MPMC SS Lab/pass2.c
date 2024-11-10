28230661e768f886a0aa92a4fe597864f5156f3a
meenakshysudhir <ukms2004@gmail.com>
2024-11-10 10:55:37 +0530

    changes to pass2.c

--- MPMC SS Lab/pass2.c
+++ MPMC SS Lab/pass2.c
@@ -11,6 +11,7 @@ struct Optab
 int programLength;
 int coptab = 0;
 int csymtab = 0;
+int txtrec = 0;
 
 struct Symtab
 {
@@ -18,58 +19,125 @@ struct Symtab
     char locctr[10];
 } symtab[100];
 
-void addtoOptab(FILE* fp,struct Optab (*op)[100]){
-    
-   char opcode[100], mnemonic[100]; 
-   while(fscanf(fp,"%s\t%s",opcode,mnemonic) != EOF)
-   {
-       strcpy((*op)[coptab].opcode,opcode);
-       strcpy((*op)[coptab].mnemonic,mnemonic);
-       coptab++;
-   }
+void addtoOptab(FILE *fp, struct Optab (*op)[100])
+{
+
+    char opcode[100], mnemonic[100];
+    while (fscanf(fp, "%s\t%s", opcode, mnemonic) != EOF)
+    {
+        strcpy((*op)[coptab].opcode, opcode);
+        strcpy((*op)[coptab].mnemonic, mnemonic);
+        coptab++;
+    }
 }
-void addtoSymtab(FILE* fp,struct Symtab (*sym)[100]){
-    
-   char label[100], locctr[100]; 
-   while(fscanf(fp,"%s\t%s",label,locctr) != EOF)
-   {
-       strcpy((*sym)[csymtab].label,label);
-       strcpy((*sym)[csymtab].locctr,locctr);
-       csymtab++;
-   }
+void addtoSymtab(FILE *fp, struct Symtab (*sym)[100])
+{
+
+    char label[100], locctr[100];
+    while (fscanf(fp, "%s\t%s", label, locctr) != EOF)
+    {
+        strcpy((*sym)[csymtab].label, label);
+        strcpy((*sym)[csymtab].locctr, locctr);
+        csymtab++;
+    }
 }
-int main(void){
+
+int searchOptab(char opcode[100])
+{
+    for (int i = 0; i < coptab; i++)
+    {
+        if (strcmp(opcode, optab[i].opcode) == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int searchSymtab(char opcode[])
+{
+    for (int i = 0; i < csymtab; i++)
+    {
+        if (strcmp(opcode, symtab[i].label) == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main(void)
+{
     FILE *f1 = fopen("intermediate.txt", "r");
-    FILE *f2 = fopen("symtab.txt","r");
-    FILE* f3 = fopen("optab.txt","r");
-    FILE* f4 = fopen("length.txt","r");
-    FILE* f5 = fopen("ObjectProgram.txt","w+");
+    FILE *f2 = fopen("symtab.txt", "r");
+    FILE *f3 = fopen("optab.txt", "r");
+    FILE *f4 = fopen("length.txt", "r");
+    FILE *f5 = fopen("ObjectProgram.txt", "w+");
+    FILE *f6 = fopen("objectCode.txt", "w+");
 
-    addtoOptab(f3,&optab);
-    addtoSymtab(f2,&symtab);
+    addtoOptab(f3, &optab);
+    addtoSymtab(f2, &symtab);
 
-    char label[100],opcode[100],operand[100],locctr[100],length[100],startingAddress[100];
-    fscanf(f4,"%s %s",length,startingAddress);
-    fscanf(f1,"%s\t%s\t%s\t%s",label,opcode,locctr,operand);
-    printf("%s %s %s",label,opcode,locctr);
-    if(strlen(label) > 6)
+    char label[100], opcode[100], operand[100], locctr[100], length[100], startingAddress[100];
+    char progName[100], mnemonic[100], opAddress[100], rec[100];
+    fscanf(f4, "%s %s", length, startingAddress);
+    fscanf(f1, "%s\t%s\t%s\t%s", label, opcode, locctr, operand);
+    fprintf(f6, "%s\t%s\t%s\t%s", locctr, label, opcode, operand);
+    if (strlen(label) > 6)
     {
         printf("Error! Program name too long\n");
     }
-    printf("%s",opcode);
-    int rest = 6 - strlen(opcode);
-    if(rest!= 0)
+    int rest = 6 - strlen(label);
+
+    if (rest != 0)
     {
-        for(int i = 0; i < rest;i++)
+        for (int i = 0; i < rest; i++)
         {
-            strcat("_",opcode);
+            strcat(progName, "_");
         }
     }
-    printf("%s",opcode);
-    //if(strcmp(opcode,"START") == 0){
-    //    printf("H^")
-    //}
-
-    
-
+    strcat(progName, label);
+    if (strcmp(opcode, "START") == 0)
+    {
+        fprintf(f5, "H^%s^%s^%s\n", progName, startingAddress, length);
+    }
+    fscanf(f1, "%s\t%s\t%s\t%s", label, opcode, operand, locctr);
+    int toCheck;
+    while (strcmp(opcode, "END") != 0)
+    {
+        int toCheck = searchOptab(opcode);
+        if (toCheck != -1) // Opcode present in Optab
+        {
+            strcpy(mnemonic, optab[toCheck].mnemonic);
+            int ind = searchSymtab(operand);
+            if (ind != -1)
+            {
+                strcpy(opAddress, symtab[ind].locctr);
+            }
+            else
+            {
+                printf("Error ! Symbol not found in symtab\n");
+                for (int i = 0; i < 4; i++)
+                {
+                    strcpy(opAddress, "0");
+                }
+            }
+            strcat(rec, mnemonic);
+            strcat(rec, opAddress);
+        }
+        else
+        {
+            if (!strcmp(opcode, "WORD") || !strcmp(opcode, "BYTE"))
+            {
+                strcpy(opAddress, operand);
+            }
+            else if (!strcmp(opcode, "RESW") || !strcmp(opcode, "RESB"))
+            {
+                strcpy(rec, "_");
+            }
+            else
+            {
+                printf("Error ! Invalid Opcode\n");
+            }
+        }
+        fprintf(f6, "%s\t%s\t%s\t%s\t%s\n", locctr, label, opcode, operand, rec);
+    }
 }
